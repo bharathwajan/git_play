253281d2d1f6c9f121c19b2c13e731e8f0fd019b
Bharathwajan R <44544547+bharathwajan@users.noreply.github.com>
2024-01-28 16:25:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 def addition(a,b):
     return int(a)+int(b)
 def multiplication(a,b):
-    """
-    this is used to do multiplication operation
-    """
     return int(a)*int(b)
-def power(a,b):
+def power(a,b,c):
     return str(int(a)**int(b))
+    
