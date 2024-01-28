dfc40bd193644321f1a9fe45b0fda221c002c40e
Bharathwajan R <44544547+bharathwajan@users.noreply.github.com>
2024-01-28 15:58:00 +0530

    notes added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def addition(a,b):
     return int(a)+int(b)
 def multiplication(a,b):
+    """
+    this is used to do multiplication operation
+    """
     return int(a)*int(b)
 def new_power(a,b):
     return int(a)**int(b)
